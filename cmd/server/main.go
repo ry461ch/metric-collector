0f33cfe40605fc2e5e617ebeb133c3cb739b3f24
ry461ch <ryabov.kolya20013@gmail.com>
2024-06-04 23:58:24 +0400

    fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"net/http"
-	"strings"
 	"strconv"
+	"strings"
 )
 
-
 type Middleware func(http.Handler) http.Handler
-
 
 type memStorage struct {
 	counter map[string]int64
@@ -17,46 +15,46 @@ type memStorage struct {
 
 var metrics = memStorage{
 	counter: make(map[string]int64),
-	gauge: make(map[string]float64)}
+	gauge:   make(map[string]float64)}
 
 func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
-    for _, middleware := range middlewares {
-        h = middleware(h)
-    }
-    return h
+	for _, middleware := range middlewares {
+		h = middleware(h)
+	}
+	return h
 }
 
 func middlewareAllowMethodPost(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		if req.Method != http.MethodPost {
-			http.Error(res, "Неверный метод", http.StatusMethodNotAllowed)
+			res.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 
-        next.ServeHTTP(res, req)
-    })
+		next.ServeHTTP(res, req)
+	})
 }
 
 func middlewareValidateRequestData(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		args := strings.Split(req.URL.Path, "/")
 
 		if len(args) != 2 {
-			http.Error(res, "Некорректный формат тела запроса", http.StatusNotFound)
+			res.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-        next.ServeHTTP(res, req)
-    })
+		next.ServeHTTP(res, req)
+	})
 }
 
 func gaugeHandler(res http.ResponseWriter, req *http.Request) {
 	args := strings.Split(req.URL.Path, "/")
 	val, err := strconv.ParseFloat(args[1], 64)
 	if err != nil {
-        http.Error(res, "Некорректный формат значения метрики", http.StatusBadRequest)
+		res.WriteHeader(http.StatusBadRequest)
 		return
-    }
+	}
 	metrics.gauge[args[0]] = val
 	res.WriteHeader(http.StatusOK)
 }
@@ -65,21 +63,17 @@ func counterHandler(res http.ResponseWriter, req *http.Request) {
 	args := strings.Split(req.URL.Path, "/")
 	val, err := strconv.ParseInt(args[1], 10, 0)
 	if err != nil {
-        http.Error(res, "Некорректный формат значения метрики", http.StatusBadRequest)
+		res.WriteHeader(http.StatusBadRequest)
 		return
-    }
+	}
 	metrics.counter[args[0]] += val
 	res.WriteHeader(http.StatusOK)
-}
-
-func invalidURLHandler(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Некорректный тип тела запроса", http.StatusNotFound)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle(
-		`/update/gauge/`, 
+		`/update/gauge/`,
 		http.StripPrefix(`/update/gauge/`, Conveyor(
 			http.HandlerFunc(gaugeHandler),
 			middlewareAllowMethodPost,
@@ -90,7 +84,9 @@ func main() {
 			http.HandlerFunc(counterHandler),
 			middlewareAllowMethodPost,
 			middlewareValidateRequestData)))
-	mux.Handle(`/update/`, http.HandlerFunc(invalidURLHandler))
+	mux.HandleFunc(`/update/`, func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(http.StatusNotFound)
+	})
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
